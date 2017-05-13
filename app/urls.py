3e3a9f73d020459b8eef6e38f328d901a0f87e5e
Mabu <manailengmj@gmail.com>
2017-05-14 01:40:20 +0200

    urls changed. mac rejects linux reg ex.

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,4 @@
 from django.conf.urls import url
-from app.views import Dashboard, Analysis, Financials, Settings, Help
-
 
 urlpatterns = [
-    url(r'^\Z', Dashboard.as_view()),
-    url(r'^dashboard$', Dashboard.as_view()),
-    url(r'^analysis$', Analysis.as_view()),
-    url(r'^financials$', Financials.as_view()),
-    url(r'^settings$', Settings.as_view()),
-    url(r'^help$', Help.as_view()),
 ]
