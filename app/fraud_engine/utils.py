25e7d3727f092cecac178eb71e7e07ab89ceefca
Mabu <manailengmj@gmail.com>
2017-05-14 18:42:21 +0200

    UX: modal design 1

--- app/fraud_engine/utils.py
+++ app/fraud_engine/utils.py
@@ -59,12 +59,14 @@ class LoadData:
 
 class DataSample(object):
     def __new__(self):
+        self.ytest = super(DataSample, self).__new__(self)
+        
         attribs = ['Xtrain', 'ytrain', 'Xtest', 'ytest', 'Xsim', 'ysim']
+        
         if not self._hasattrs(attribs):
             self.Xtrain = super(DataSample, self).__new__(self)
             self.ytrain = super(DataSample, self).__new__(self)
             self.Xtest = super(DataSample, self).__new__(self)
-            self.ytest = super(DataSample, self).__new__(self)
             self.Xsim = super(DataSample, self).__new__(self)
             self.ysim = super(DataSample, self).__new__(self)
             self._setdata(LoadData().get_data())
@@ -81,9 +83,12 @@ class DataSample(object):
 
     @classmethod
     def _setdata(self, data):
+        """
         y = data.Class.values
         X = data.drop('Class', axis=1).values
         self.Xtrain, X_test, self.ytrain, y_test = \
             train_test_split(X, y, test_size=0.4)
         self.Xtest, self.Xsim, self.ytest, self.ysim = \
             train_test_split(X_test, y_test, test_size=0.1)
+        """
+
