63f53bd8270f726c19ae32937f3b803675e6d389
Mabu <manailengmj@gmail.com>
2017-05-17 14:39:05 +0200

    Mabu

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from app.views import Help, UpdateTrasaction, LockTrasaction, UnlockTrasaction
-from app.views import Dashboard, Analysis, Financials, Settings,Demo
+from app.views import Dashboard, Analysis, Financials, Settings, Demo
 
 
 uuid_pattern = '[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12}'
@@ -13,9 +13,9 @@ urlpatterns = [
     url(r'^financials/$', Financials.as_view(), name='financials'),
     url(r'^settings/$', Settings.as_view(), name='settings'),
     url(r'^help/$', Help.as_view(), name='help'),
+    url(r'^demo/$', Demo.as_view(), name='demo'),
     url(r'^accept/(?P<ac>[0-9]+)/', UpdateTrasaction.as_view(),name='accept'),
     url(r'^lock/(?P<ac>[0-9]+)/', LockTrasaction.as_view(), name='lock'),
     url(r'^unlock/(?P<ac>[0-9]+)/', UnlockTrasaction.as_view(), name='unlock'),
     # url(r'^report/(?P<>[0-9]+)/',UpdateTrasaction.as_view(), name='report'),
-    url(r'^demo/$', Demo.as_view(), name='demo'),
 ]
